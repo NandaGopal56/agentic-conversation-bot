e941bedeb3812bf62f0e4ec0b4463f6730845835
NandaGopal56 <ngopal561998@gmail.com>
2026-01-02 23:10:46 +0530

    promt fixes

--- agents/graph/workflow.py
+++ agents/graph/workflow.py
@@ -16,8 +16,8 @@ from .nodes import (
     tool_node_processor,
     video_capture,
     path_selector_post_llm_call,
-    fanout_selector,      # NEW
-    join_after_tools      # NEW
+    fanout_selector, 
+    join_after_tools 
 )
 
 def build_workflow() -> StateGraph:
